2462fac744666d61d061c84663093fd1681d18c1
Pablo Joaquim <pablo.joaquim@famar.com>
2022-12-30 20:01:52 -0300

    The hello-world program has been added.

---
+++ hello_world/src/main.rs
@@ -0,0 +1,10 @@
+// This code has been created using "cargo new hello-world"
+
+// Functions are introduced with fn.
+// Blocks are delimited by curly braces like in C and C++.
+// The main function is the entry point of the program.
+// Rust has hygienic macros, println! is an example of this.
+// Rust strings are UTF-8 encoded and can contain any Unicode character.
+fn main() {
+    println!("Hello, world!");
+}
